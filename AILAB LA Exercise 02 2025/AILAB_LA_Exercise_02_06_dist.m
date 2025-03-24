db662d91a6740570ef5ffd94b5d8103890af0064
thejourneyofbabo <journeyofbabo@gmail.com>
2025-03-24 11:55:40 +0900

    exercise

--- AILAB LA Exercise 02 2025/AILAB_LA_Exercise_02_06_dist.m
+++ AILAB LA Exercise 02 2025/AILAB_LA_Exercise_02_06_dist.m
@@ -6,20 +6,20 @@ s = 2;
 
 %%%%%%% TODO %%%%%%%
 % Add scalar s to vector v 
-v_plus_s = % x + x;
+v_plus_s = v + s;
 %%%%%%%%%%%%%%%%%%%%
 
 % Create figure
 figure;
 
 % Display vector v from the origin
-quiver(0, 0, v(1), v(2), 'b', 'LineWidth', 2);
+quiver(0, 0, v(1), v(2), 0, 'b', 'LineWidth', 2);
 hold on;
 
 
 
 % Display vector v + scalar ('v_plus_s') from the origin
-quiver(0, 0, v_plus_s(1), v_plus_s(2), 'r', 'LineWidth', 2);
+quiver(0, 0, v_plus_s(1), v_plus_s(2), 0, 'r', 'LineWidth', 2);
 
 % Set axes
 axis equal;
