008dcd6062d9a6b788e9ca3c3045797bf13082e3
thejourneyofbabo <journeyofbabo@gmail.com>
2025-04-22 22:41:14 +0900

    .

--- Practice_ws/LinearAlgebra_LiveScripts_ws/test.m
+++ Practice_ws/LinearAlgebra_LiveScripts_ws/test.m
@@ -1,7 +1,24 @@
-A = [[1 4]; [2 8]];
-Apinv = pinv(A);
-AApinv = A*Apinv;
-disp("Apinv");
-disp(Apinv);
-disp("AApinv");
-disp(AApinv);
+close all; clc; clear;
+
+y = -1:0.1:1;
+x = zeros(size(y));
+
+points = [x; y];
+
+theta = -pi/5;
+
+T = [cos(theta), sin(theta);
+    -sin(theta), cos(theta)];
+
+r_points = T * points;
+
+figure;
+hold on;
+
+plot(points(1,:), points(2,:), 'b.', DisplayName='Original Points', MarkerSize=15);
+
+plot(r_points(1,:), r_points(2,:), 'r.', DisplayName='Rotated Points', MarkerSize=15);
+
+grid on;
+xlim([-1 1]);
+ylim([-1 1]);
